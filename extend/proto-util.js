9b24bf90823e70afac6dd6f582118eada7d9c068
ft <fengtong@mail.ustc.edu.cn>
2015-03-16 13:46:10 +0800

    add proto util

---
+++ extend/proto-util.js
@@ -0,0 +1,58 @@
+var $ = require('$')
+var _ = require('min-util')
+
+var is = _.is
+
+$.fn.extend({
+	  toArray: function() {
+		return $.toArray(this)
+	}
+	, pushStack: function(nodes) {
+		var ret = $(nodes)
+		ret.prevObject = this
+		ret.context = this.context
+		return ret
+	}
+	, get: function(i) {
+		if (!is.num(i)) return this.toArray()
+		if (i > 0) {
+			return this[i]
+		}
+		return this.get(i + this.length)
+	}
+	, each: function(fn) {
+		_.each(this, function(val, i) {
+			return fn.call(val, i, val, this)
+		})
+		return this
+	}
+	, map: function(fn) {
+		var arr = _.map(this, function(val, i) {
+			arr[i] = fn.call(val, i, val, this)
+		})
+		return this.pushStack(arr)
+	}
+	, filter: function(fn) {
+		var arr = _.filter(this, function(val, i) {
+			return fn.call(val, i, val, this)
+		})
+		return this.pushStack(arr)
+	}
+	, end: function() {
+		return this.prevObject || $()
+	}
+	, eq: function(i) {
+		if (i < 0) return this.eq(i + this.length)
+		return this.pushStack([this[i]])
+	}
+	, first: function() {
+		return this.eq(0)
+	}
+	, last: function() {
+		return this.eq(-1)
+	}
+	, slice: function(start, end) {
+		var arr = _.slice(this, start, end)
+		return this.pushStack(arr)
+	}
+})
